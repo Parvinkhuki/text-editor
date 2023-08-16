62e06ee981f00f4d3262c58d8b41f6f3d3b88eaa
Parvinkhuki <khuki.developer@gmail.com>
2023-08-17 04:14:41 +0600

    style done

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const texts=document.getElementById("text")
+const textJustify=document.getElementById("just")
+const textCenter=document.getElementById("center")
+const textLeft=document.getElementById("left")
+const textBold=document.getElementById("bold")
+const textItalic=document.getElementById("italic")
+const textUnder=document.getElementById("under")
+
+
+textBold.addEventListener('click',function(){
+    texts.style.background="blue"
+} )
